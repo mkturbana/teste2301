45e24531af51aec612be48016779438fa8f00a6a
mkturbana <mkturbana@gmail.com>
2025-01-23 16:26:27 -0300

    Update parse-xml.js

--- my-wati-api/api/utils/parse-xml.js
+++ my-wati-api/api/utils/parse-xml.js
@@ -1,1 +1,27 @@
+const axios = require('axios');
+const { parseStringPromise } = require('xml2js');
+
+async function parseXML(xmlURL, propertyCode) {
+  try {
+    const response = await axios.get(xmlURL);
+    const xmlData = response.data;
+
+    const jsonData = await parseStringPromise(xmlData);
+    const properties = jsonData.Listings.Listing || [];
+    const property = properties.find(item => item.ListingID[0] === propertyCode);
+
+    if (!property) return null;
+
+    return {
+      provider: property.Provider[0],
+      contactName: property.ContactName[0],
+      telephone: property.Telephone[0],
+    };
+  } catch (error) {
+    console.error('Erro ao processar XML:', error);
+    return null;
+  }
+}
+
+module.exports = { parseXML };
 
