108d07fe3c6c44f2b3e3b24f433662608f05a8e5
mkturbana <mkturbana@gmail.com>
2025-01-23 16:26:00 -0300

    Update extract-link.js

--- my-wati-api/api/utils/extract-link.js
+++ my-wati-api/api/utils/extract-link.js
@@ -1,1 +1,8 @@
+function extractLink(message) {
+  const urlRegex = /(https?:\/\/[^\s]+)/g;
+  const matches = message.match(urlRegex);
+  return matches ? matches[0] : null;
+}
+
+module.exports = { extractLink };
 
