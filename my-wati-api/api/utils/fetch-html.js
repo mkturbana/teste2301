6d28b08a1405a3b8e17d7ba931ccb6e5816abeb4
mkturbana <mkturbana@gmail.com>
2025-01-23 16:26:14 -0300

    Update fetch-html.js

--- my-wati-api/api/utils/fetch-html.js
+++ my-wati-api/api/utils/fetch-html.js
@@ -1,1 +1,14 @@
+const axios = require('axios');
+
+async function fetchHTML(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao buscar HTML:', error);
+    return null;
+  }
+}
+
+module.exports = { fetchHTML };
 
